ee2853b4288d1591fbefe767a6a69c5db0da6a5b
keriyason <keriyason@lcad.edu>
2025-11-23 14:47:56 -0800

    weapon + enemy script changes
    weapon script
    change enemy script to inculde takedmg

---
+++ Fih/Assets/Keriya Scripts/Weapon.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public int damage = 1; // damage per hit
+    public float swingAngle = 90f; // how far the player will swing the weapon
+    public float swingSpeed = 10f; // speed of swing
+
+    private bool swingLeft = true;
+    private bool isSwinging = false;
+
+    private Quaternion startRotation;
+    private Quaternion targetRotation;
+
+
+    private void Start()
+    {
+        startRotation = transform.localRotation;
+    }
+    private void Update()
+    {
+       if (Input.GetMouseButton(0) && !isSwinging)
+        {
+            StartCoroutine(Swing());
+        }
+    }
+    private System.Collections.IEnumerator Swing()
+    {
+        isSwinging = true;
+
+       
+        float angle = swingLeft ? swingAngle : -swingAngle;
+        targetRotation = Quaternion.Euler(0f, angle, 0f) * startRotation;
+
+        
+        float t = 0f;
+        while (t < 1f)
+        {
+            t += Time.deltaTime * swingSpeed;
+            transform.localRotation = Quaternion.Slerp(startRotation, targetRotation, t);
+            yield return null;
+        }
+
+        transform.localRotation = startRotation;
+
+        swingLeft = !swingLeft;
+        isSwinging = false;
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("enemy"))
+        {
+            Enemy enemy = other.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+        }
+    }
+}
+
+
--- Fih/Assets/coopers scripts/enemy.cs
+++ Fih/Assets/coopers scripts/enemy.cs
@@ -1,28 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 public class Enemy : parent
 {
     [SerializeField] private Transform Player;
-    
+
     public NavMeshAgent agent;
-public int health = 2;
+    public int health = 2;
 
- private void Start()
+    private void Start()
     {
-         agent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
     }
-        void Update()
-{
-    if (Player != null)       
+    void Update()
     {
-        agent.SetDestination(Player.position);
-    }
-    else
-    {
-        agent.ResetPath(); 
-        Debug.Log("enemy at rest");
+        if (Player != null)
+        {
+            agent.SetDestination(Player.position);
+        }
+        else
+        {
+            agent.ResetPath();
+            Debug.Log("enemy at rest");
+
+        }
 
     }
-}}
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        Debug.Log(gameObject.name + " took " + amount + " damage. Remaining health: " + health);
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
